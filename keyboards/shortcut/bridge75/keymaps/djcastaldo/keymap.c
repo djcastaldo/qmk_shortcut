9d7615cd394035d4293b3c738d892d81bf87d5e8
Dave Castaldo <djcastaldo@gmail.com>
2026-03-21 23:15:38 -0400

    use unique timer and tap_count var names per keycode to prevent conflicts; update RCTL tap dance

--- keyboards/shortcut/bridge75/keymaps/djcastaldo/keymap.c
+++ keyboards/shortcut/bridge75/keymaps/djcastaldo/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,  KC_P, KC_LBRC, KC_RBRC,  DYN_LT,      KC_PGUP,
         TD(CAPS_LAYR),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L, KC_SCLN, KC_QUOT,    KC_ENT,      KC_PGDN,
         KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(RSFT_OSL), KC_UP,  OSL_FNSYM,
-        KC_LCTL, TD(LGUI_OSL), KC_LALT,                     KC_SPC,                    TD(RALT_OSL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, TD(LGUI_OSL), KC_LALT,                     KC_SPC,                    TD(RALT_OSL), SP_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
 //  [MAC_BASE_LAYR]
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T,  KC_Y, KC_U, KC_I, KC_O, KC_P,KC_LBRC,KC_RBRC, LT(EMO_LAYR,KC_BSLS),KC_PGUP,
         TD(CAPS_LAYR),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L, KC_SCLN, KC_QUOT,    KC_ENT,      KC_PGDN,
         KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(RSFT_OSL), KC_UP,  OSL_FNSYM,
-        KC_LCTL, KC_LOPT, KC_LCMD,                        KC_SPC,                      TD(RCMD_OSL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LOPT, KC_LCMD,                        KC_SPC,                      TD(RCMD_OSL), SP_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
 //  [CLCK_LAYR]
